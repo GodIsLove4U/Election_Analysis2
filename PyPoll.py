e96c903427718a355a7da7db1d22b40922ef0736
GodIsLove4U <61957567+GodIsLove4U@users.noreply.github.com>
2020-03-28 16:08:25 -0700

    Write to Files
    Lesson 3.4.3 exercises

--- PyPoll.py
+++ PyPoll.py
@@ -30,3 +30,15 @@ with open(file_to_load) as election_data:
 
         # Print the file object.
         print(election_data)
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_filewrite("Hello World")
+
+# Write three counties to the file.
+txt_file.write"Arapahoe")
