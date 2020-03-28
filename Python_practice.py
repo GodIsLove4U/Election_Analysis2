6b7e533526cce2380ba680845fed11a73a21766f
GodIsLove4U <61957567+GodIsLove4U@users.noreply.github.com>
2020-03-27 21:04:35 -0700

    RepetitionStatements2
    I did not have much success in this module, as VS Code did not appear to be recognizing my statements. :(

--- Python_practice.py
+++ Python_practice.py
@@ -1,16 +1,2 @@
-print("Hello World")
-
-counties = ["Arapahoe","Denver","Jefferson"]
-if counties[1] =='Denver':
-    print(counties[1])
-counties = ["Arapahoe", "Denver", "Jefferson"]
-if "El Paso" in counties:
-    print("El Paso is in the list of counties.")
-else:
-    print("El Paso is not in the list of counties")
-
-
-if "Arapahoe" in counties and "El Paso" not counties:
-    print("Only Arapahoe is in the list of counties.")
-else:
-    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+for county in counties_dict:
+    print(counties_dict.get(county))
