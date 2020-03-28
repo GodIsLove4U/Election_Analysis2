647a3a8a23e944fb3036a1ee3a4f87ec8c2cd2e2
GodIsLove4U <61957567+GodIsLove4U@users.noreply.github.com>
2020-03-27 20:22:19 -0700

    Logical Operators
    Exercises in 3.2.9

--- Python_practice.py
+++ Python_practice.py
@@ -8,3 +8,9 @@ if "El Paso" in counties:
     print("El Paso is in the list of counties.")
 else:
     print("El Paso is not in the list of counties")
+
+
+if "Arapahoe" in counties and "El Paso" not counties:
+    print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
