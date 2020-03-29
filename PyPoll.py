e28f91158ec977cce1fee6f7bb79e109c6d30c3e
GodIsLove4U <61957567+GodIsLove4U@users.noreply.github.com>
2020-03-28 22:09:44 -0700

    Election Results
    Lesson 3.6.1

--- PyPoll.py
+++ PyPoll.py
@@ -1,56 +1,59 @@
-# Import the datetime class from the datetime module.
-import datetime as dt
-# Use the now() attribute on the datetime class to get the present time.
-now = dt.datetime.now()
-# Print the present time.
-print("The time right now is,", now)
-
-# Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
-
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-
-    # To do: perform analysis
-    print(election_data)
-
-# To do: perform analysis.
-
-# Close the file.
-election_data.close()
-
+# Add our dependencies.
 import csv
 import os
-# Assign a variable for the file to load and the path.
-file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources/election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Initialize a total vote counter.
+total_votes = 0
+# Candidate options and candidate votes
+candidate_options = []
+candidate_votes = {}
+# Track the winning candidate, vote count, and percentage.
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # Open the election results and read the file.
-# with open(file_to_load) as election_data:
-
-    # Print the file object
-    #print(election_data)
-
-# Create a filename variable to a direct or indirect path to the file.
-#file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-#open(file_to_save, "w")
-
-# Create a file variable to a direct or indirect path to the file.
-with open (file_to_load) as election_data:
-
-# Using the with statement open the file as a text file.
-#with open(file_to_save, "w") as txt_file:
-
-    # Write three counties to the file.
-    # txt_file.write("Counties in the Election\n-------------------\nArapahoe\nDenver\nJefferson")
-
-    # To do: read and analyze the data here.
-
-    # Read the file object with the reader funcition.
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
-
-    # Print header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+        # Get the candidate name from each row.
+        candidate_name = row[2]
+        # If the candidate does not match any existing candidate add it the
+        # the candidate list.
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            # And begin tracking that candidate's voter count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count
+        candidate_votes[candidate_name] += 1
 
-# Close the file
-csv.reader.close()
+for candidate in candidate_votes:
+    # Retrieve vote count and percentage.
+    votes = candidate_votes[candidate]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # Print each candidate, their voter count, and percentage to the
+    # terminal.
+    print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count, winning percentage, and candidate.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_candidate = candidate
+        winning_percentage = vote_percentage
+# Print the winning candidates' results to the terminal.
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
